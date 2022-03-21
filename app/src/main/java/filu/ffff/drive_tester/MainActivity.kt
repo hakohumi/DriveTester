b90f2d64a9db8d0602fa8bfb80d078983fcebbdb
filu <mihumihu32@gmail.com>
2022-03-21 12:51:19 +0900

    ジャイロ追加、メーター追加

--- app/src/main/java/filu/ffff/drive_tester/MainActivity.kt
+++ app/src/main/java/filu/ffff/drive_tester/MainActivity.kt
@@ -10,9 +10,14 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,21 +25,27 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import filu.ffff.drive_tester.ui.theme.DriveTesterTheme
 
 class MainActivity : ComponentActivity(), SensorEventListener {
     // 遅延する必要がある
     //　SensorManagerをgetSystemServiceから生成するには、アクティビティ上で呼ぶ必要がある
     private lateinit var sensorManager: SensorManager
-    lateinit var sensor: Sensor
-    var onGet: (value: Float) -> Unit = {}
+    private lateinit var accSensor: Sensor
+    private lateinit var gyroSensor: Sensor
+    var onGetAcc: (value: Float) -> Unit = {}
+    var onGetGyro: (value: Float) -> Unit = {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-            ?: throw IllegalStateException("センサーが見つかりませんでした。")
+        accSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+            ?: throw IllegalStateException("加速度センサーが見つかりませんでした。")
+        gyroSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+            ?: throw IllegalStateException("ジャイロセンサーが見つかりませんでした。")
         Log.d("main", "sensor")
 
         setContent {
@@ -44,13 +55,36 @@ class MainActivity : ComponentActivity(), SensorEventListener {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val angle = remember { mutableStateOf(0.0f) }
-                    onGet = {
-                        angle.value = it
+                    val accAngle = remember { mutableStateOf(0.0f) }
+                    onGetAcc = {
+                        accAngle.value = it
                     }
 
-                    UpdateNeedle(angle.value)
 
+                    val gyroTotal = remember { mutableStateOf(0.0f) }
+                    onGetGyro = {
+                        gyroTotal.value += -it
+                    }
+
+                    val padding = 16.dp
+                    Column(
+                        modifier = Modifier
+                            .padding(padding)
+                            .fillMaxHeight()
+//                            .fillMaxWidth()
+//                            .fillMaxSize()
+                    ) {
+                        Text(text = "Acc: ${accAngle.value}")
+                        UpdateMeter(accAngle.value, modifier = Modifier.weight(1f))
+                        Text(text = "Gyro: ${gyroTotal.value}")
+                        UpdateMeter(gyroTotal.value, modifier = Modifier.weight(1f))
+                        Button({
+                            accAngle.value = 0.0f
+                            gyroTotal.value = 0.0f
+                        }) {
+                            Text(text = "Reset")
+                        }
+                    }
                     Log.d("main", "created")
                 }
             }
@@ -59,7 +93,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(this, gyroSensor, SensorManager.SENSOR_DELAY_FASTEST)
 
         Log.d("dddds", "onresume")
     }
@@ -72,11 +107,23 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
-//        Log.d("values", "${event?.values?.map { it.toString() }}")
         event ?: return
 
-        onGet(event.values[0])
+        Log.d("sensor", "${event.sensor.type}: ${event.values.map { it.toString() }}")
 
+        when (event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                // X軸方向の加速力
+                onGetAcc(event.values[0])
+            }
+            Sensor.TYPE_GYROSCOPE -> {
+                // y軸周りの回転速度
+                onGetGyro(event.values[2])
+            }
+            else -> {
+                // なにもしない
+            }
+        }
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
@@ -84,13 +131,18 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     }
 }
 
+
 @Composable
-fun UpdateNeedle(angle: Float) {
+fun UpdateMeter(angle: Float, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(id = R.drawable.needle1),
-        contentDescription = "needle", modifier = Modifier.graphicsLayer(
-            rotationZ = angle
-        )
+        contentDescription = "needle",
+        modifier = modifier
+            .graphicsLayer(
+                rotationZ = angle
+            )
+//            .fillMaxWidth()
+            .fillMaxSize()
     )
 }
 
